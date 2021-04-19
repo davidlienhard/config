2434ce532a98eb955664c7b763b52ec0cbc2e71b
David Lienhard <43143441+davidlienhard@users.noreply.github.com>
2021-04-19 09:49:54 +0200

    update author (#51)
     - update composer
     - update copyright & author in source

--- src/ConfigInterface.php
+++ src/ConfigInterface.php
@@ -3,8 +3,8 @@
  * contains \DavidLienhard\Config\ConfigInterface
  *
  * @package         tourBase
- * @author          David Lienhard <david.lienhard@tourasia.ch>
- * @copyright       tourasia
+ * @author          David Lienhard <github@lienhard.win>
+ * @copyright       David Lienhard
  */
 
 declare(strict_types=1);
@@ -14,16 +14,16 @@ namespace DavidLienhard\Config;
 /**
  * interface for tourBase configuration object
  *
- * @author          David Lienhard <david.lienhard@tourasia.ch>
- * @copyright       tourasia
+ * @author          David Lienhard <github@lienhard.win>
+ * @copyright       David Lienhard
  */
 interface ConfigInterface
 {
     /**
      * sets path containing configuration files
      *
-     * @author          David Lienhard <david.lienhard@tourasia.ch>
-     * @copyright       tourasia
+     * @author          David Lienhard <github@lienhard.win>
+     * @copyright       David Lienhard
      * @param           string          $directory      directory containing json configuration file
      * @return          void
      */
@@ -32,8 +32,8 @@ interface ConfigInterface
     /**
      * returns the required configuration and loads it once
      *
-     * @author          David Lienhard <david.lienhard@tourasia.ch>
-     * @copyright       tourasia
+     * @author          David Lienhard <github@lienhard.win>
+     * @copyright       David Lienhard
      * @param           string          $mainKey        the main key of the configuration. will be used as filename
      * @param           string          $subKeys        keys that will be used to find the config
      */
@@ -42,8 +42,8 @@ interface ConfigInterface
     /**
      * returns the current log-directory
      *
-     * @author          David Lienhard <david.lienhard@tourasia.ch>
-     * @copyright       tourasia
+     * @author          David Lienhard <github@lienhard.win>
+     * @copyright       David Lienhard
      */
     public function getDirectory() : string;
 }
